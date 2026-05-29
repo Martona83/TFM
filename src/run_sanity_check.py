33316a6618220e52ce1fa5c86cb506e4d88349a5
Marta Alberola Martin <marta.alberola@gmail.com>
2026-05-29 11:52:46 +0200

    tokens and checks reversion

--- src/run_sanity_check.py
+++ src/run_sanity_check.py
@@ -2,6 +2,7 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 from fairness_pipeline.workflow import run_full_pipeline
 =======
 =======
@@ -14,6 +15,9 @@ except ModuleNotFoundError:
     from src.workflow import run_full_pipeline
 
 >>>>>>> parent of ee2e379 (Potential fix for pull request finding)
+=======
+from fairness_pipeline.workflow import run_full_pipeline
+>>>>>>> parent of a3da1b2 (ci: create uv-based workflow and harden sanity check import)
 =======
 from fairness_pipeline.workflow import run_full_pipeline
 >>>>>>> parent of a3da1b2 (ci: create uv-based workflow and harden sanity check import)
